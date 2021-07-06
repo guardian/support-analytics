b674abe35dc12faf4ebb53fbc79a52b0f24f8caa
Tom Forbes <tom.forbes@theguardian.com>
2021-07-06 09:27:43 +0100

    bucket name

--- sql/create-aggregated-epic-views-table.sql
+++ sql/create-aggregated-epic-views-table.sql
@@ -5,6 +5,6 @@ CREATE EXTERNAL TABLE IF NOT EXISTS acquisition.aggregated-epic-views-prod (
 ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
 WITH SERDEPROPERTIES (
   'serialization.format' = '1'
-) LOCATION 's3://support-aggregated-epic-views/PROD/'
+) LOCATION 's3://gu-support-analytics/aggregated-epic-views/PROD/'
 TBLPROPERTIES ('has_encrypted_data'='false');
 
