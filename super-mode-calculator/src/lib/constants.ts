cb5408ebdddaed1e4069b92200ea07d001970e86
Tom Forbes <tom.forbes@theguardian.com>
2021-08-04 12:10:21 +0100

    Update thresholds

--- super-mode-calculator/src/lib/constants.ts
+++ super-mode-calculator/src/lib/constants.ts
@@ -8,12 +8,12 @@ export const SUPER_MODE_DURATION_IN_HOURS = 24;
 export const SUPER_MODE_MINIMUM_VIEWS = 100;
 
 // Minimum av an article needs for it to be eligible for super mode
-export const SUPER_MODE_MINIMUM_AV = 50;
+export const SUPER_MODE_MINIMUM_AV = 40;
 
 export const SUPER_MODE_AV_PER_VIEWS_THRESHOLDS = {
-	GB: 0.02,
+	GB: 0.015,
 	US: 0.05,
-	AU: 0.05,
+	AU: 0.03,
 	NZ: 0.05,
 	CA: 0.05,
 	EU: 0.03,
