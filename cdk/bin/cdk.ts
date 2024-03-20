6a9a3bb9e77272cd34e9136cf5814f141eb29b3d
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-03-20 15:24:38 +0000

    fix stack name!

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,12 +4,12 @@ import { Bandit } from '../lib/bandit';
 
 const app = new GuRoot();
 new Bandit(app, 'Bandit-CODE', {
-	stack: 'deploy',
+	stack: 'support',
 	stage: 'CODE',
 	env: { region: 'eu-west-1' },
 });
 new Bandit(app, 'Bandit-PROD', {
-	stack: 'deploy',
+	stack: 'support',
 	stage: 'PROD',
 	env: { region: 'eu-west-1' },
 });
