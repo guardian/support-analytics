ee377f88e87246b7ec8c5f8694c54dcd4602de1a
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-03-20 12:19:43 +0000

    update name

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,15 @@
-import "source-map-support/register";
-import { GuRoot } from "@guardian/cdk/lib/constructs/root";
-import { Bandit } from "../lib/bandit";
+import 'source-map-support/register';
+import { GuRoot } from '@guardian/cdk/lib/constructs/root';
+import { Bandit } from '../lib/bandit';
 
 const app = new GuRoot();
-new Bandit(app, "Bandit-euwest-1-CODE", { stack: "deploy", stage: "CODE", env: { region: "eu-west-1" } });
-new Bandit(app, "Bandit-euwest-1-PROD", { stack: "deploy", stage: "PROD", env: { region: "eu-west-1" } });
+new Bandit(app, 'Bandit-CODE', {
+	stack: 'deploy',
+	stage: 'CODE',
+	env: { region: 'eu-west-1' },
+});
+new Bandit(app, 'Bandit-PROD', {
+	stack: 'deploy',
+	stage: 'PROD',
+	env: { region: 'eu-west-1' },
+});
