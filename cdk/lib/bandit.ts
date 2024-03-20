52b248f82a7d37e2c7e189b461614546fae77911
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-03-20 14:47:59 +0000

    remove imports for previous commented code

--- cdk/lib/bandit.ts
+++ cdk/lib/bandit.ts
@@ -1,9 +1,7 @@
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
-import { type App, RemovalPolicy } from 'aws-cdk-lib';
-import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { type App } from 'aws-cdk-lib';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import {
 	DefinitionBody,
