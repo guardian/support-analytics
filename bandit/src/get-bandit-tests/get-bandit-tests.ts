560a2a93b09d59015b13a9a014e65bf18bb3ed29
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2024-03-21 16:20:25 +0000

    add type to lambda handler function

--- bandit/src/get-bandit-tests/get-bandit-tests.ts
+++ bandit/src/get-bandit-tests/get-bandit-tests.ts
@@ -1,11 +1,12 @@
 import * as AWS from "aws-sdk";
+import type { Test } from "../lib/models";
 import { queryChannelTests } from "./dynamo";
 
 const STAGE: string = process.env.Stage ?? "PROD";
 
 const docClient = new AWS.DynamoDB.DocumentClient({ region: "eu-west-1" });
 
-export async function run() {
+export async function run(): Promise<Test[]> {
 	const banditTests = await queryChannelTests(STAGE, docClient);
-	return banditTests.Items;
+	return banditTests.Items as Test[];
 }
