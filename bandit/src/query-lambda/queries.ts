44184ee2e0b6c601c952e14ea97471fdd34499a2
Tom Forbes <tom.forbes@theguardian.com>
2024-03-26 11:53:59 +0000

    precise intervals

--- bandit/src/query-lambda/queries.ts
+++ bandit/src/query-lambda/queries.ts
@@ -1,12 +1,15 @@
-import { format } from "date-fns";
+import { format, set, subHours } from "date-fns";
 import type { Test } from "../lib/models";
 import { Query } from "../lib/query";
 
-// TODO - rewrite query to use real data to get sum of views and sum of AV GBP
 const buildQuery = (test: Test, stage: "CODE" | "PROD"): Query => {
-	const oneHourAgo = new Date();
-	oneHourAgo.setHours(oneHourAgo.getHours() - 1);
-	const oneHourAgoTimestamp = oneHourAgo.toISOString().replace("T", " ");
+	const end = set(
+		new Date(),
+		{ minutes: 0, seconds: 0, milliseconds: 0 },
+	);
+	const start = subHours(end, 1);
+	const endTimestamp = end.toISOString().replace("T", " ");
+	const startTimestamp = start.toISOString().replace("T", " ");
 
 	const query = `
 		WITH views AS (
@@ -15,8 +18,8 @@ const buildQuery = (test: Test, stage: "CODE" | "PROD"): Query => {
 				ab.variant variant_name,
 				COUNT(*) views
 			FROM acquisition.epic_views_prod, UNNEST(abtests) t(ab)
-			WHERE 
-			AND date_hour >=  timestamp'${oneHourAgoTimestamp}'
+			WHERE
+			AND date_hour >= timestamp'${startTimestamp}' AND date_hour < timestamp'${endTimestamp}'
 			AND ab.name = '${test.name}'
 			GROUP BY 1,2
 		),
@@ -27,8 +30,8 @@ const buildQuery = (test: Test, stage: "CODE" | "PROD"): Query => {
 				SUM(annualisedvaluegbp) av_gbp,
 				COUNT(*) acquisitions
 			FROM acquisition.acquisition_events_prod, UNNEST(abtests) t(ab)
-			WHERE acquisition_date >= date'${format(oneHourAgo, "yyyy-MM-dd")}'
-			AND timestamp >=  timestamp'${oneHourAgoTimestamp}'
+			WHERE acquisition_date >= date'${format(start, "yyyy-MM-dd")}'
+			AND timestamp >= timestamp'${startTimestamp}' AND timestamp < timestamp'${endTimestamp}'
 			AND ab.name = '${test.name}'
 			GROUP BY 1,2
 		)
@@ -42,7 +45,7 @@ const buildQuery = (test: Test, stage: "CODE" | "PROD"): Query => {
 		JOIN acquisitions USING (test_name, variant_name)
 	`;
 
-	return new Query(query, `query_${test.name}_${oneHourAgo.toISOString()}`);
+	return new Query(query, `query_${test.name}_${start.toISOString()}`);
 };
 
 export const getQueries = (
