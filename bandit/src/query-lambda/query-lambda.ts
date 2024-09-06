c7f7c42bfb7bdbc9349056a381afc03f7ba37173
“LAKSHMIRPILLAI” <luxmi.r.pillai@gmail.com>
2024-09-06 14:38:49 +0100

    Test the parsed Result

--- bandit/src/query-lambda/query-lambda.ts
+++ bandit/src/query-lambda/query-lambda.ts
@@ -11,6 +11,8 @@ const athena = new AWS.Athena({ region: "eu-west-1" });
 const stage = process.env.STAGE;
 const athenaOutputBucket = process.env.AthenaOutputBucket ?? "";
 const schemaName = "acquisition";
+const docClient = new AWS.DynamoDB.DocumentClient({ region: "eu-west-1" });
+
 
 export interface QueryLambdaInput {
 	tests: Test[];
@@ -23,17 +25,46 @@ export async function run(input: QueryLambdaInput): Promise<QueryExecution[]> {
 	}
 
 	const ssmPath = `/bandit-testing/${stage}/gcp-wif-credentials-config`;
-
+	const date = input.date ?? new Date(Date.now());
+	const end = set(date, { minutes: 0, seconds: 0, milliseconds: 0 });
+	const start = subHours(end, 1);
+	const startTimestamp = start.toISOString().replace("T", " ");
 	const bigQueryData= await getSSMParam(ssmPath)
 		.then(buildAuthClient)
 		.then(authClient =>
 			banditTestingData(authClient, stage, input));
 
-	console.log("bigQueryData", bigQueryData);
 
-	const date = input.date ?? new Date(Date.now());
-	const end = set(date, { minutes: 0, seconds: 0, milliseconds: 0 });
-	const start = subHours(end, 1);
+	const testNamesResult =  bigQueryData.map((data) => data.testName);
+	const variantResult =  bigQueryData.map((data) => data.rows);
+	console.log("bigQueryData", bigQueryData);
+	console.log("bigQueryData Testname Result", testNamesResult);
+	console.log("bigQueryData Variant Result", variantResult);
+
+
+	if (testNamesResult.length !== variantResult.length) {
+		throw new Error('Arrays must have the same length.');
+	}
+
+	for (let i = 0; i < testNamesResult.length; i++) {
+		const testName = testNamesResult[i];
+		const variant= variantResult[i];
+		// const parsedVariant = parseResultFromBigQuery(variant);
+		// console.log("parsedVariantResult", parsedVariant);
+		//
+		console.log("Final Data", testName,variant,startTimestamp);
+		// buildWriteRequest(
+		// 	parsedVariant,
+		// 	testName,
+		// 	startTimestamp
+		// );
+
+	}
+
+
+
+
+
 
 	const queries = getQueries(input.tests, stage, start, end);
 
