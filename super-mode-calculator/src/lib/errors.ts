032ad1a5339659c8f756e43993a3585357504bf1
“LAKSHMIRPILLAI” <luxmi.r.pillai@gmail.com>
2024-12-16 12:36:21 +0000

    Delete query.ts and unwanted errors

--- super-mode-calculator/src/lib/errors.ts
+++ super-mode-calculator/src/lib/errors.ts
@@ -1,23 +1,3 @@
-export class InvalidConfigError extends Error {
-	name = 'InvalidConfigError';
-	message = 'Invalid config';
-}
-
-export class QueryExecutionNotFoundError extends Error {
-	name = 'QueryExecutionNotFoundError';
-	message = 'Query execution not found';
-}
-
-export class QueryPendingError extends Error {
-	name = 'QueryPendingError';
-	message = 'Query pending';
-}
-
-export class QueryFailedError extends Error {
-	name = 'QueryFailedError';
-	message = 'Query failed';
-}
-
 export class QueryReturnedInvalidDataError extends Error {
 	name = 'QueryReturnedInvalidDataError';
 	message = 'Query returned invalid data';
--- super-mode-calculator/src/lib/query.ts
+++
@@ -1,9 +0,0 @@
-export class Query {
-	query: string;
-	token: string;
-
-	constructor(q: string, name: string) {
-		this.query = q;
-		this.token = `${name}_${new Date().toISOString()}`;
-	}
-}
